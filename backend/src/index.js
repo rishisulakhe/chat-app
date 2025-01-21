2f12e43cebd970e69c01bfd8df9c0fecefb11f86
rishisulakhe <rishiprasadsulakhe@gmail.com>
2025-01-21 13:06:42 +0530

    Production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,9 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 import cors from "cors"
 import { app ,server } from './lib/socket.js';
-
+import path from 'path';
+const PORT=process.env.PORT;
+const __dirname=path.resolve();
 dotenv.config();
 app.use(express.json())
 app.use(express.json({ limit: '50mb' })); // Set the limit as needed (e.g., '50mb', '100mb')
@@ -23,9 +25,16 @@ app.use(cors({
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+    app.get("*",(req,res){
+        res.sendFile(path.join(__dirname,"../../frontend","dist","index.html"))
+    })
+}
 
-const PORT=process.env.PORT;
+
+
 server.listen(PORT,()=>{
-    console.log("Server is running on portal 3000");
+    console.log(`Server is running on port ${process.env.PORT}`);
     connectDB();
 })
